e947e9b847fc38bb6c374e1c492235d657a5fdce
qrbys <qrbys@outlook.com>
2015-07-22 13:05:23 +0900

    add Ajax version

---
+++ not Ajax/initialize.php
@@ -0,0 +1,6 @@
+<?PHP
+#初期設定を共通化して手間を省く(hogeとfoo,barは差し替え必須)
+define('CONSUMER_KEY', 'hogehogehoge');
+define('CONSUMER_SECRET','foofoofoo');
+define('OAUTH_CALLBACK', 'http://localhost/barbarbarbar/callback.php');
+?>
